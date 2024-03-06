2af8d8207b1cd0cd1eb03c3357fcc5069923e295
Juan <juandi.21.jdj@gmail.com>
2024-03-06 11:17:54 -0500

    Add link for vercel on index.js api

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser())
 app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: ['http://localhost:5173', 'https://airbnb-clone-jdj.vercel.app'], 
 }))
 
 mongoose.connect(process.env.MONGO_URL)
